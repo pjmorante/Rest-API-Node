17e33584120c2283f4baf5e7aa24918e65e55e39
Pedro Morante <pedro.morante@outlook.com>
2024-11-10 22:49:47 -0500

    add put, patch and delete endpoints

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -37,3 +37,52 @@ export const createProduct = async (req: Request, res: Response) => {
   const product = await Product.create(req.body);
   res.json({ data: product });
 };
+
+export const updateProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await Product.findByPk(id);
+
+  if (!product) {
+     res.status(404).json({
+      error: 'Producto No Encontrado',
+    });
+    return;
+  }
+
+  // Actualizar
+  await product.update(req.body);
+  await product.save();
+  res.json({ data: product });
+};
+
+export const updateAvailability = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await Product.findByPk(id);
+
+  if (!product) {
+     res.status(404).json({
+      error: 'Producto No Encontrado',
+    });
+    return;
+  }
+
+  // Actualizar
+  product.availability = !product.dataValues.availability;
+  await product.save();
+  res.json({ data: product });
+};
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await Product.findByPk(id);
+
+  if (!product) {
+     res.status(404).json({
+      error: 'Producto No Encontrado',
+    });
+    return;
+  }
+
+  await product.destroy();
+  res.json({ data: 'Producto Eliminado' });
+};
--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createProduct, getProductById, getProducts } from "./handlers/product";
+import { createProduct, deleteProduct, getProductById, getProducts, updateAvailability, updateProduct } from "./handlers/product";
 import { body, param } from "express-validator";
 import { handleInputErrors } from "./middleware";
 
@@ -26,6 +26,40 @@ router.post('/',
       .custom(value => value > 0).withMessage('Precio no válido'),  
   handleInputErrors,
   createProduct
+);
+
+router.put(
+  '/:id',
+  param('id').isInt().withMessage('ID no válido'),
+  body('name')
+    .notEmpty()
+    .withMessage('El nombre de Producto no puede ir vacio'),
+  body('price')
+    .isNumeric()
+    .withMessage('Valor no válido')
+    .notEmpty()
+    .withMessage('El precio de Producto no puede ir vacio')
+    .custom((value) => value > 0)
+    .withMessage('Precio no válido'),
+  body('availability')
+    .isBoolean()
+    .withMessage('Valor para disponibilidad no válido'),
+  handleInputErrors,
+  updateProduct
+);
+
+router.patch(
+  '/:id',
+  param('id').isInt().withMessage('ID no válido'),
+  handleInputErrors,
+  updateAvailability
+);
+
+router.delete(
+  '/:id',
+  param('id').isInt().withMessage('ID no válido'),
+  handleInputErrors,
+  deleteProduct
 );
 
 export default router;
