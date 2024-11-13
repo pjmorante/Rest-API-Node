34f173ccd7733409a8c309e84a9b7ffc776b2ce1
Pedro Morante <pedro.morante@outlook.com>
2024-11-12 22:50:55 -0500

    set cors up

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,13 @@ connectDB();
 const server = express();
 
 const corsOptions : CorsOptions = {
-
+  origin: function(origin, callback) {
+    if (origin === process.env.FRONTEND_URL) {
+      callback(null, true)
+    } else {
+      callback(new Error('Error de Cors'));
+    }
+  }
 }
 server.use(cors(corsOptions));
 
